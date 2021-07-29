36e5fceaeea311c1525076e2de4d2d4bf954b68f
Bri <bhilliker@gmail.com>
2021-07-29 15:14:31 -0500

    added inquirer to prompt user

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require ('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = []
+
+inquirer
+    .prompt([
+        {
+            type:'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
