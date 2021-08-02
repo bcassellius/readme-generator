b854d85acd34a4ac18be192279b32243cf20ffe5
Bri <bhilliker@gmail.com>
2021-08-01 22:10:07 -0500

    added more questions for the user prompt

--- index.js
+++ index.js
@@ -1,94 +1,128 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require ('fs');
-const profileDataArgs = process.argv.slice(2);
-const [username, github] = profileDataArgs;
 const generateMarkdown = require ('./utils/generateMarkdown');
-const generatePage = require('./src/page-template.js');
-fs.writeFile('index.md', generatePage(username, github), err => {
-    if (err) throw err;
-  
-    // console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
 
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of your project? (Required)',
-        validate: projectName => {
-          if (projectName){
-            return true;
-          } else {
-            console.log('Please provide the name of your project!')
-          }
-        }
-    },
-    {
-        type: 'input',
-        name: 'link',
-        message: 'Enter the GitHub username associated with your project. (Required)',
-        validate: githubInput => {
-          if (githubInput) {
-            return true;
-          } else {
-            console.log('Please enter the link to your project!')
-          }
-        }
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of the project. (Required)',
-        validate: projectDescription => {
-            if (projectDescription){
-            return true;
-            } else {
-            console.log('Please provide a description of the project!')
-            }
-        }
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Which license would you like to use?',
-        choices: ['one', 'two', 'three', 'four'],
-        
-    },
-    {
-        type:'input',
-        name: 'questions',
-        message: 'Enter an email address where you can be reached. (Required)',
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log('Please enter the email address where you can be reached about questions on your project!')
-            }
-        }        
-    },  
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project? (Required)',
+    validate: projectName => {
+      if (projectName){
+        return true;
+      } else {
+        console.log('Please provide the title of your project!')
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of the project. (Required)',
+    validate: projectDescription => {
+      if (projectDescription){
+        return true;
+      } else {
+        console.log('Please provide a description of the project!')
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide instructions for application installation.'
+  },
+  { type: 'input',
+    name: 'usage',
+    message: 'Provide information for application usage.',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Which license would you like to use?',
+    choices: ['MIT', 'GNU', 'GPLv3', 'Unlicense'],
+  },
+  {
+    type: 'input',
+    name: 'link',
+    message: 'Enter the GitHub username associated with your project. (Required)',
+    validate: githubInput => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log('Please enter the link to your project!')
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide information on tests for your app here.'
+  },
+  {
+    type:'input',
+    name: 'questions',
+    message: 'Enter an email address where questions can be sent. (Required)',
+    validate: emailInput => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log('Please enter the email address where you can be reached about questions on your project!')
+      }
+    }        
+  },  
 ]
 
+
 function showAnswers() {
-    inquirer.prompt(questions)
-    .then(inquirerResponses => {
-         console.log(inquirerResponses)
-    }
-)} 
+  inquirer.prompt(questions)
+  .then(inquirerResponses => {
+    console.log(inquirerResponses)
+  }
+)}; 
 
 showAnswers();
+  
+  
+// // TODO: Create a function to write README file
+// // this will write the file
+// const writeFile = fileContent => {
+//   return new Promise((resolve, reject) => {
+//     fs.writeFile('./dist/index.md', fileContent, err => {
+//       // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+//       if (err) {
+//         reject (err);
+//         // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+//         return;
+//       }    
+//       resolve({
+//         ok: true,
+//         message: 'File created!'
+//       });
+//     });
+//   });
+// };
+// function writeToFile(fileName, data) {
+// }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
 
 
+// // TODO: Create a function to initialize app
+// function init() {
+//   return inquirer
+//   .prompt(questions)
+//   .then(response => {
+//     readmeObj = response
+//   })
+// }
 
-// // record video windows + g
+// // Function call to initialize app
+// init();
+
+
+
+// // // record video windows + g
